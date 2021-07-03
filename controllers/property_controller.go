ba4019b940f72c07ec391c7c183dd0a43c3e8fbe
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-07-03 13:11:29 +0800

    Add preload to find all associated couuntry in a property

--- controllers/property_controller.go
+++ controllers/property_controller.go
@@ -1,24 +1,35 @@
 package controllers
 
 import (
-	"errors"
 	"log"
 	"strconv"
 
 	"github.com/gofiber/fiber"
 	"github.com/mofodox/omh-rest-api/database"
 	"github.com/mofodox/omh-rest-api/models"
-	"gorm.io/gorm"
 )
 
 func AddProperty(ctx *fiber.Ctx) {
 	var property models.Property
 
 	if err := ctx.BodyParser(&property); err != nil {
-		log.Fatalln(err)
+		ctx.Status(442).JSON(fiber.Map{
+			"code": 442,
+			"message": "Unable to process your JSON request",
+		})
 	}
 
 	database.DB.Create(&property)
+
+	if property.ID != 0 {
+		err := database.DB.Debug().Model(&models.Country{}).Where("id = ?", property.CountryID).Take(&property.Country).Error
+		if err != nil {
+			ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"code": fiber.StatusInternalServerError,
+				"message": "Create porperty: server error",
+			})
+		}
+	}
 
 	ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"code": fiber.StatusCreated,
@@ -37,7 +48,7 @@ func ShowAllProperties(ctx *fiber.Ctx) {
 	} else if modeQuery == "Sale" {
 		database.DB.Model(&models.Property{}).Where("Mode = ?", modeQuery).Find(&properties)
 	} else {
-		database.DB.Find(&properties)
+		database.DB.Preload("Country").Find(&properties)
 	}
 
 	ctx.Status(fiber.StatusOK).JSON(fiber.Map{
@@ -49,10 +60,18 @@ func ShowAllProperties(ctx *fiber.Ctx) {
 }
 
 func ShowProperty(ctx *fiber.Ctx) {
-	id := ctx.Params("ID")
+	id, err := strconv.ParseUint(ctx.Params("id"), 10, 64)
+	if err != nil {
+		ctx.Status(442).JSON(fiber.Map{
+			"code": 442,
+			"message": "Unable to process your JSON request",
+		})
+	}
 
 	var property models.Property
-	database.DB.Find(&property, id)
+	db := database.DB.Find(&property, id)
+
+	log.Printf("GET Property - Rows affected: %v\n", db.RowsAffected)
 
 	ctx.Status(fiber.StatusOK).JSON(fiber.Map{
 		"code": fiber.StatusOK,
@@ -64,45 +83,61 @@ func ShowProperty(ctx *fiber.Ctx) {
 
 // TODO: Update handler for property still a work in progress
 func UpdateProperty(ctx *fiber.Ctx) {
-	id, _ := strconv.Atoi(ctx.Params("ID"))
-
 	property := new(models.Property)
-	
-	err := ctx.BodyParser(&property)
+
+	if err := ctx.BodyParser(property); err != nil {
+		ctx.Status(422).JSON(fiber.Map{
+			"code": 422,
+			"message": "Unable to parse json values",
+		})
+
+		return
+	}
+
+	id, err := strconv.ParseUint(ctx.Params("id"), 10, 64)
 	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = database.DB.Model(&property).Where("ID = ?", id).Updates(&property).Find(&property, id).Error
-
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"code": fiber.StatusNotFound,
-			"message": "Property not found",
-		})
-	} else {
-		ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"code": fiber.StatusInternalServerError,
-			"message": "Internal server error",
+		ctx.Status(442).JSON(fiber.Map{
+			"code": 442,
+			"message": "error converting the id to int64",
 		})
 	}
-	
+
+	var propertyDB models.Property
+	database.DB.First(&propertyDB, id)
+	db := database.DB.Model(&propertyDB).Updates(map[string]interface{}{
+		"Address": property.Address,
+		"Bedroom": property.Bedroom,
+		"Bathroom": property.Bathroom,
+		"Price": property.Price,
+		"Sqm": property.Sqm,
+		"Mode": property.Mode,
+		"HomeType": property.HomeType,
+	})
+
+	log.Printf("PUT - Rows affected: %v\n", db.RowsAffected)
+
 	ctx.Status(fiber.StatusOK).JSON(fiber.Map{
 		"code": fiber.StatusOK,
 		"message": "Property updated successfully",
-		"property": property,
+		"property": propertyDB,
 	})
 }
 
 func DeleteProperty(ctx *fiber.Ctx) {
-	id := ctx.Params("ID")
+	id, err := strconv.ParseUint(ctx.Params("id"), 10, 64)
+	if err != nil {
+		ctx.Status(442).JSON(fiber.Map{
+			"code": 442,
+			"message": "error converting the id to int64",
+		})
+	}
 	
 	var property models.Property
 
 	database.DB.Find(&property, id)
 	database.DB.Delete(&property)
 
-	ctx.Status(fiber.StatusSeeOther).JSON(fiber.Map{
+	ctx.Status(fiber.StatusNoContent).JSON(fiber.Map{
 		"message": "Property successfully deleted",
 	})
 }
