2da69afe7d1b3d7e7cf3439d24793b7e1aac1953
Khairul Akmal <khairulakmal.dev@gmail.com>
2021-07-03 13:31:20 +0800

    Load env file in init function

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/fiber/middleware"
+	"github.com/joho/godotenv"
 	"github.com/mofodox/omh-rest-api/database"
 	"github.com/mofodox/omh-rest-api/routes"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("unable to load values from env file")
+	} else {
+		log.Println("successfully loaded the values from env file")
+	}
+}
 
 func main() {
 	app := fiber.New()
